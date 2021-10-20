657555b4bd3c71a3e576486dfb397548add4d8fc
VadimShegolev <keepman12121998@gmail.com>
2021-10-20 10:28:32 +0600

    button isnt working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import './style.scss';
 
-var message = "";
-
-function send() {
-    var msg = document.getElementById("uname");
-    message = msg.outerHTML;
-    console.log(message);
-}
-
 console.log("index.ts is working");
+
+var bttn = document.getElementById("button");
+var form = document.getElementById("form");
+
+function handleForm(event: any) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+bttn.onclick = function(){
+    var text = (document.getElementById('textbox') as HTMLFormElement).value;
+    console.log(text);
+    (document.getElementById('textbox') as HTMLFormElement).value = null;
+}
